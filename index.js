3b54d646ad1b598e86f24d84644e7c9ec6b9c4e6
Ken Vilar <kenvilar@gmail.com>
2026-06-15 17:47:02 +0800

    Remove auto-update current year function
    Removed the function that auto-updates the current year text.

--- index.js
+++ index.js
@@ -11,14 +11,4 @@ document.addEventListener("DOMContentLoaded", () => {
     `);
   }
   printLMASignature();
-
-  // Auto Update Current Year Text (Vanilla JS)
-  function setLMACurrentYear() {
-    const year = new Date().getFullYear();
-
-    document.querySelectorAll(".year-today").forEach((el) => {
-      el.textContent = year;
-    });
-  }
-  setLMACurrentYear();
 });
